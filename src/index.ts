51f04e4a3fbf1b44f59fbc3679433a5a0f897a40
nsc2820 <nsc2820@mkone.co.kr>
2025-08-31 17:13:27 +0900

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 export { default as MyButton } from "./external/button/MyButton";
 export { default as CacheProvider } from "./external/redux/CacheProvider";
-export { default as CachedComponent } from "./external/redux/CachedComponent";
 export { default as useCache } from "./external/redux/useCache";
