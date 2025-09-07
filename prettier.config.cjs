9ef6750bbc9eb85cca1924fdb75edd37a3dcc888
nsc2820 <nsc2820@mkone.co.kr>
2025-09-07 14:51:52 +0900

    feat: add prettier

---
+++ prettier.config.cjs
@@ -0,0 +1,11 @@
+// prettier.config.js
+module.exports = {
+    printWidth: 100, // 한 줄 최대 길이
+    tabWidth: 4, // 들여쓰기 4칸
+    useTabs: false, // 탭 대신 스페이스
+    semi: true, // 세미콜론 사용
+    singleQuote: true, // 작은 따옴표 사용
+    trailingComma: 'es5', // ES5 호환 가능 쉼표
+    bracketSpacing: true, // 객체 리터럴 내 공백
+    arrowParens: 'always', // 화살표 함수 매개변수 항상 괄호
+};
